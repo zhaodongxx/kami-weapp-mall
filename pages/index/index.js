baebee4a486921fc6d8d382d77637b1666a57312
zhaodong <zhaodongxx@outlook.com>
2019-03-16 20:48:28 +0800

    增加首页banner

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,17 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    circular:true, //衔接滑动
+    interval: 3000,
+    duration: 1000
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,14 +25,28 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-  },
+  onLoad: function() {},
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  changeIndicatorDots(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
     })
   }
 })
